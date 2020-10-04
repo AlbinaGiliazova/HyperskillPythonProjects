1951a83844463da9e678c39ee79f03297b2ec9a3
Albina <29810425+AlbinaGiliazova@users.noreply.github.com>
2020-10-04 23:28:37 +0300

    Update TextBasedBrowser.py

--- TextBasedBrowser.py
+++ TextBasedBrowser.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 A text-based browser that can show the text from the requested URLs, parsed 
-for particular HTML tags; colour the links in blue; return back to the 
+for the particular HTML tags; colour the links in blue; return back to the 
 previous tabs; save the tabs in a directory and show the cached tabs. The 
 commands are: "back", "exit", an Internet address with or without "https://".
 
