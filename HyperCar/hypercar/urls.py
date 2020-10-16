d5efe176a971074090d74a79b4c368e8688f1c8b
Albina <29810425+AlbinaGiliazova@users.noreply.github.com>
2020-10-16 15:26:38 +0300

    Create urls.py

---
+++ HyperCar/hypercar/urls.py
@@ -0,0 +1,28 @@
+"""hypercar URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, re_path
+from tickets.views import WelcomeView, MenuView, TicketView,\
+    ProcessingView, NextView
+
+
+urlpatterns = [
+    path("", MenuView.as_view()),
+    re_path('welcome/?', WelcomeView.as_view()),
+    re_path('menu/?', MenuView.as_view()),
+    path('get_ticket/<str:link>', TicketView.as_view()),
+    re_path('processing/?', ProcessingView.as_view()),
+    re_path('next/?', NextView.as_view()),
+]
