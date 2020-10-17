e77e82c93192435daea96b618164a3eff97592ed
Albina <29810425+AlbinaGiliazova@users.noreply.github.com>
2020-10-17 21:46:37 +0300

    Create views.py

---
+++ HyperJob/resume/views.py
@@ -0,0 +1,30 @@
+from django.views.generic.base import TemplateView
+from resume.models import Resume
+from django.views import View
+from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
+
+class MainView(TemplateView):
+    template_name = "resume\\main.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['resumes'] = Resume.objects.all()
+        return context
+
+class NewView(View):
+
+    def get(self, request, *args, **kwargs):
+        return redirect("/home")
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        if request.user.is_staff:
+            return HttpResponseForbidden()
+        # return HttpResponseForbidden('<h1>403 Forbidden</h1>', content_type='text/html')
+        description = request.POST.get("description")
+        author = request.user
+        Resume.objects.create(description=description,
+                              author=author)
+        return redirect("/home")
